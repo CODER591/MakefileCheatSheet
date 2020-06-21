b1ecb7df09d41a3166b8433614de91adf8e8d847
CODER591 <mazorchyk@gmail.com>
2020-06-21 15:27:36 +0300

    Finished Example2

---
+++ Example2/kek_dir/kek.h
@@ -0,0 +1,7 @@
+#include <iostream>
+class Kek {
+
+public:
+  Kek();
+  void show_kek();
+};
--- Example2/main_dir/main.cpp
+++ Example2/main_dir/main.cpp
@@ -2,11 +2,13 @@
 
 #include <iostream>
 #include "exp.h"
-
+#include "kek.h"
 using namespace std;
 
-int main(){ 
- 
+int main(){
+
   Example1();
+  Kek obj;
+  obj.show_kek();
   return 0;
 }
