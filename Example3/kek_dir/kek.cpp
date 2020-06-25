db776df5c4e5cab5014f7af5e23e726389271557
CODER591 <mazorchyk@gmail.com>
2020-06-25 22:04:21 +0300

    Added description comment to Example2 makefile

---
+++ Example3/kek_dir/kek.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "kek.h"
+Kek::Kek() {
+
+}
+
+void Kek::show_kek() {
+  std::cout<<"Kek"<<std::endl;
+}
---
+++ Example3/main_dir/main.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+#include "exp.h"
+#include "kek.h"
+using namespace std;
+
+int main(){
+
+  Example1();
+  Kek obj;
+  obj.show_kek();
+  return 0;
+}
